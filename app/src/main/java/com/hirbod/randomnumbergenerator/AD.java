875ab106683b58e5147d68651875fd2fd2674e0c
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2019-07-29 13:01:41 +0430

    Replaced ads with Google ADMob

--- app/src/main/java/com/hirbod/randomnumbergenerator/AD.java
+++ app/src/main/java/com/hirbod/randomnumbergenerator/AD.java
@@ -1,71 +1,58 @@
 package com.hirbod.randomnumbergenerator;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.LinearLayout;
 
-import ir.tapsell.sdk.Tapsell;
-import ir.tapsell.sdk.TapsellAd;
-import ir.tapsell.sdk.TapsellAdRequestListener;
-import ir.tapsell.sdk.TapsellAdRequestOptions;
-import ir.tapsell.sdk.TapsellAdShowListener;
-import ir.tapsell.sdk.TapsellShowOptions;
-import ir.tapsell.sdk.bannerads.TapsellBannerType;
-import ir.tapsell.sdk.bannerads.TapsellBannerView;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+import java.util.Random;
+
 
 class AD {
-    private static TapsellAd tapsellAd = null;
-    static void InitAd(final Activity activity){
+    private static InterstitialAd mInterstitialAd;
 
+    static void InitAd(final Activity activity){
+        MobileAds.initialize(activity, "ca-app-pub-4493333048205720~5460376984");
     }
     static void LoadFullScreenAd(final Activity activity){
-        TapsellAdRequestOptions options = new TapsellAdRequestOptions();
-        options.setCacheType(TapsellAdRequestOptions.CACHE_TYPE_STREAMED);
-        Tapsell.requestAd(activity, "5cfd1c8daf72830001b77f5f", options, new TapsellAdRequestListener() {
+        mInterstitialAd = new InterstitialAd(activity);
+        mInterstitialAd.setAdUnitId("ca-app-pub-4493333048205720/6905320597");
+        mInterstitialAd.setAdListener(new AdListener() {
             @Override
-            public void onError(String s) {
-
+            public void onAdClosed() {
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
             }
 
             @Override
-            public void onAdAvailable(TapsellAd tapsellAd) {
-                AD.tapsellAd = tapsellAd;
+            public void onAdLoaded() {
+                Log.v("ADS","InterstitialAd Loaded");
             }
 
             @Override
-            public void onNoAdAvailable() {
-
-            }
-
-            @Override
-            public void onNoNetwork() {
-
-            }
-
-            @Override
-            public void onExpiring(TapsellAd tapsellAd) {
-
+            public void onAdFailedToLoad(int i) {
+                Log.d("ADS","InterstitialAd error: "+ i);
             }
         });
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
     }
     static void ShowFullScreenAD(final Activity activity){
-        if(tapsellAd != null)
-            tapsellAd.show(activity, new TapsellShowOptions(), new TapsellAdShowListener() {
-                @Override
-                public void onOpened(TapsellAd tapsellAd) {
-                    AD.tapsellAd = null;
-                    LoadFullScreenAd(activity);
-                }
-
-                @Override
-                public void onClosed(TapsellAd tapsellAd) {
-                    AD.tapsellAd = null;
-                    LoadFullScreenAd(activity);
-                }
-            });
+        if (mInterstitialAd.isLoaded())
+            if(new Random().nextBoolean())
+                mInterstitialAd.show();
     }
     static void LoadBanner(final Activity activity){
-        TapsellBannerView banner = new TapsellBannerView(activity, TapsellBannerType.BANNER_320x50,"5cfd01d63ea54800014dd610");
-        banner.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        ((LinearLayout) activity.findViewById(R.id.BottomOfPageLayout)).addView(banner);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        AdView adView = new AdView(activity);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-4493333048205720/5209095549");
+        adView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT));
+        ((LinearLayout) activity.findViewById(R.id.BottomOfPageLayout)).addView(adView);
+        adView.loadAd(adRequest);
     }
 }
